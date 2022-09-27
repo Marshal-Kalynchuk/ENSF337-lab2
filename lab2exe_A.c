5b968d708d7b72fd9a2b42aa6b1b74b84479cfcd
Marshal <marshalkalynchuk1@gmail.com>
2022-09-27 11:56:40 -0600

    Added create table function

--- lab2exe_A.c
+++ lab2exe_A.c
@@ -44,6 +44,12 @@ int main(void)
     create_table(velocity);
     return 0;
 }
+double create_table(double v) {
+    printf("|Angle|Maximum horizontal distance|Maximum travel time|");
+    for (int i = 0; i =< 90; i++) {
+        printf("|%5d|%5lf|%5lf|", i, Projectile_travel_distance(i, velocity), Projectile_travel_time(i, velocity));
+    }
+}
 
 double degree_to_radian(double d) {
     return d * PI / 180;
